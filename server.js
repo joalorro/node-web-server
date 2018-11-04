c6b861d02cefd33731b2d68808c6ad3c6f01b930
joalorro <joalorro@gmail.com>
2018-11-04 18:35:53 -0500

    configuring app to run on heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -53,6 +55,8 @@ app.get('/bad', (req, resp) => {
 	})
 })
 
-app.listen(3000, () => {
-	console.log('Server running on port 3000')
+
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
 })
